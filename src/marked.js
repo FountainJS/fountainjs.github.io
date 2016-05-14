f729487aad853d2bcbbb00e31cecd5e76475afe3
Matthieu Lux <matthieu.lux@gmail.com>
2016-05-14 13:34:08 +0200

    fix default lang for prism and add gulp angular readme images

--- src/marked.js
+++ src/marked.js
@@ -1,10 +1,12 @@
 import marked from 'marked';
 import Prism from 'prismjs';
+import 'prismjs/components/prism-bash';
 
 const renderer = new marked.Renderer();
 
 renderer.code = (code, lang) => {
-  const highlighted = Prism.highlight(code, Prism.languages[lang]);
+  const language = Prism.languages[lang ? lang : 'bash'];
+  const highlighted = Prism.highlight(code, language);
   return `<pre class="language-${lang}"><code>${highlighted}</code></pre>`;
 };
 
