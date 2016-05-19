25efe4ff49afcf4c77b2c54ec3475e441303bab0
Matthieu Lux <matthieu.lux@gmail.com>
2016-05-19 20:59:34 +0200

    add responsive media queries

--- src/components/home/tagline.js
+++ src/components/home/tagline.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 export default class Tagline extends Component {
   render() {
     return (
-      <p className="tagline">FountainJS aims to bring a rich DX by making you forget about JS fatigue</p>
+      <div className="container">
+        <p className="tagline">FountainJS aims to bring a rich DX by making you forget about JS fatigue</p>
+      </div>
     );
   }
 }
