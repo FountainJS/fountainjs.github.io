1898a2322e69f7d1cafa6dbf33eecb2d9726260d
Swiip <matthieu.lux@gmail.com>
2017-02-22 10:48:57 +0100

    update webpack, fix build, fix relative assets url

--- conf/webpack-dist.conf.js
+++ conf/webpack-dist.conf.js
@@ -3,6 +3,7 @@ const conf = require('./gulp.conf');
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FailPlugin = require('webpack-fail-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const pkg = require('../package.json');
 const autoprefixer = require('autoprefixer');
@@ -11,36 +12,37 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /.json$/,
+        test: /\.json$/,
         loaders: [
-          'json'
+          'json-loader'
         ]
       },
       {
-        test: /.js$/,
+        test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'eslint',
+        loader: 'eslint-loader',
         enforce: 'pre'
       },
       {
         test: /\.(css|scss)$/,
         loaders: ExtractTextPlugin.extract({
-          fallbackLoader: 'style',
-          loader: 'css?minimize!sass!postcss'
+          fallback: 'style-loader',
+          use: 'css-loader?minimize!sass-loader!postcss-loader'
         })
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loaders: [
-          'babel'
+          'babel-loader'
         ]
       }
     ]
   },
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    FailPlugin,
     new HtmlWebpackPlugin({
       template: conf.path.src('index.html')
     }),
@@ -48,9 +50,10 @@ module.exports = {
       'process.env.NODE_ENV': '"production"'
     }),
     new webpack.optimize.UglifyJsPlugin({
+      output: {comments: false},
       compress: {unused: true, dead_code: true, warnings: false} // eslint-disable-line camelcase
     }),
-    new ExtractTextPlugin('index-[contenthash].css'),
+    new ExtractTextPlugin('/index-[contenthash].css'),
     new webpack.optimize.CommonsChunkPlugin({name: 'vendor'}),
     new webpack.LoaderOptionsPlugin({
       options: {
@@ -60,7 +63,7 @@ module.exports = {
   ],
   output: {
     path: path.join(process.cwd(), conf.paths.dist),
-    filename: '[name]-[hash].js'
+    filename: '/[name]-[hash].js'
   },
   entry: {
     app: `./${conf.path.src('index')}`,
